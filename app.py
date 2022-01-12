2fa878bd815a97ccadeef979891c0ade4cacabf8
Joe Skvarna <jnskvarna@ucdavis.edu>
2022-01-11 23:55:16 -0800

    submission boxes set up

--- app.py
+++ app.py
@@ -15,14 +15,22 @@ db = SQLAlchemy(app)
 @app.route('/prediction',  methods = ['GET', 'POST'])
 def prediction():
     form_data = request.form
-    target = request.form.get('target')
-    shots = request.form.get('shots')
-    fouls = request.form.get('fouls')
-    corners = request.form.get('corners')
-    yellow = request.form.get('yellow')
-    red = request.form.get('red')
-    return render_template('prediction.html', form_data = form_data, target = target, shots = shots, fouls = fouls,
-    corners = corners, yellow = yellow, red = red)
+    targeth = request.form.get('targeth')
+    shotsh = request.form.get('shotsh')
+    foulsh = request.form.get('foulsh')
+    cornersh = request.form.get('cornersh')
+    yellowh = request.form.get('yellowh')
+    redh = request.form.get('redh')
+    targeta = request.form.get('targeta')
+    shotsa = request.form.get('shotsa')
+    foulsa = request.form.get('foulsa')
+    cornersa = request.form.get('cornersa')
+    yellowa = request.form.get('yellowa')
+    reda = request.form.get('reda')
+
+    return render_template('prediction.html', form_data = form_data, targeth = targeth, shotsh = shotsh, foulsh = foulsh,
+    cornersh = cornersh, yellowh = yellowh, redh = redh, targeta = targeta, shotsa = shotsa, foulsa = foulsa,
+    cornersa = cornersa, yellowa = yellowa, reda = reda)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
