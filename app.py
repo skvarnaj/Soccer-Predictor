9a3043e56266776916a06865e4dd95545c9e7c90
Joe Skvarna <jnskvarna@ucdavis.edu>
2022-01-11 23:11:38 -0800

    got submission forms up and working

--- app.py
+++ app.py
@@ -5,26 +5,28 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from forms import HomeStats, AwayStats
 from flask_bootstrap import Bootstrap
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'b43e4b51d4f030a6f240c4bf5e41bcf5'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-#db = SQLAlchemy(app)
 
-#class Todo(db.Model):
-   # id = db.Column(db.Integer, primary_key = True)
-    #content = db.Column(db.String(200), nullable = False)
+app.config['SECRET_KEY'] = 'b43e4b51d4f030a6f240c4bf5e41bcf5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db = SQLAlchemy(app)
 
 @app.route('/prediction',  methods = ['GET', 'POST'])
 def prediction():
-    return render_template('prediction.html')
+    form_data = request.form
+    target = request.form.get('target')
+    shots = request.form.get('shots')
+    fouls = request.form.get('fouls')
+    corners = request.form.get('corners')
+    yellow = request.form.get('yellow')
+    red = request.form.get('red')
+    return render_template('prediction.html', form_data = form_data, target = target, shots = shots, fouls = fouls,
+    corners = corners, yellow = yellow, red = red)
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     form = HomeStats()
-    if form.validate_on_submit:
-        flash('Thanks for submitting')
-        #return redirect(url_for('prediction'))
     return render_template('homestats.html', title = 'homestats', form = form)
 
 @app.route('/homestats', methods = ['GET', 'POST'])
