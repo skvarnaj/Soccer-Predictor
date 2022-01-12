b4a3deff688eca8497e4e36e88d27439e8d8f301
Joe Skvarna <jnskvarna@ucdavis.edu>
2022-01-11 19:14:22 -0800

    flask forms and classes made

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from forms import HomeStats, AwayStats
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'b43e4b51d4f030a6f240c4bf5e41bcf5'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+#db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    content = db.Column(db.String(200), nullable = False)
+#class Todo(db.Model):
+   # id = db.Column(db.Integer, primary_key = True)
+    #content = db.Column(db.String(200), nullable = False)
 
-@app.route("/")
+@app.route('/prediction')
+def prediction():
+    return render_template('prediction.html')
+
+@app.route("/", methods = ['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    form = HomeStats()
+    return render_template('homestats.html', title = 'homestats', form = form)
+
+@app.route('/homestats', methods = ['GET', 'POST'])
+def homestats():
+    form = HomeStats()
+    return render_template('homestats.html', title = 'homestats', form = form)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
